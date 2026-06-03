57e1bcd6605e33844fada100dd8f991c54f576d2
PIROLLEY Benoist <134169432+Dzaze@users.noreply.github.com>
2026-06-03 11:19:39 +0200

    Modify lancer_fusion.bat for improved execution
    Updated batch script to use 'py' command and added error handling.

--- lancer_fusion.bat
+++ lancer_fusion.bat
@@ -1,5 +1,9 @@
 @echo off
 REM Lance le script fusion_tool.py en mode merge
 REM Ce batch suppose que fusion_tool.py se trouve dans le même dossier que le batch
-python "%~dp0fusion_tool.py" merge mon_super_fichier.dart
+
+py "%~dp0fusion_tool.py" merge "%~dp0mon_super_fichier.dart" --project_dir "%~dp0"
+if %errorlevel% neq 0 (
+    echo ERREUR : le script a échoué avec le code %errorlevel%
+)
 pause
