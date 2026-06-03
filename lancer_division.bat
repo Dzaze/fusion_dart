eb76f6920fbe04263021d0399fd47327fea87cc4
PIROLLEY Benoist <134169432+Dzaze@users.noreply.github.com>
2026-06-03 11:20:30 +0200

    Update lancer_division.bat

--- lancer_division.bat
+++ lancer_division.bat
@@ -1,3 +1,9 @@
 @echo off
-python "%~dp0fusion_tool.py" split mon_super_fichier.dart
+REM Lance le script fusion_tool.py en mode split
+REM Ce batch suppose que fusion_tool.py se trouve dans le même dossier que le batch
+
+py "%~dp0fusion_tool.py" split "%~dp0mon_super_fichier.dart" --project_dir "%~dp0"
+if %errorlevel% neq 0 (
+    echo ERREUR : le script a échoué avec le code %errorlevel%
+)
 pause
